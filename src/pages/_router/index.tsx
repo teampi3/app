d264b6284454f59e1860a1e1e9aa8a9a442e2c25
Wesley Barbosa <wesley.barbosa.developer@gmail.com>
2025-06-21 02:41:57 -0300

    feat: add /app route

--- src/pages/_router/index.tsx
+++ src/pages/_router/index.tsx
@@ -1,8 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { HomePage } from '@/pages/home'
 import { DashboardPage } from '@/pages/dashboard'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/dashboard', element: <DashboardPage /> },
+  { path: '/app', element: <DashboardPage /> },
 ])
